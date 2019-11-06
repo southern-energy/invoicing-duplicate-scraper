574c420a00e48c9be32c6b7ef5f9a11225f99a6b
gregorywpower <gregorywpower@gmail.com>
2019-11-06 16:29:05 -0500

    first commit

---
+++ duplicate_printer.py
@@ -0,0 +1,18 @@
+# secnd_list = [["Not","Me"], ["Dash", "Final"], ["Not_It", "Left"]]
+# first_list = [["Dash", "Final"], ["Extra", "Data"]]
+
+# first_set = set(map(tuple, first_list))
+# secnd_set = set(map(tuple, secnd_list))
+
+# # Creates list of duplicates.
+
+# duplicates = first_set.intersection(secnd_set)
+
+
+# #print(first_list)
+# #print(secnd_list)
+# print(duplicates)
+
+# Above is the basic principle behind this program.
+
+# Started by Gregory Power at 11/06/19 @ 4:26 PM
