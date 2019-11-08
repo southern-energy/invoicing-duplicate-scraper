cbcfadcbdebb83f49845c19d06398a553dc828ea
gregorywpower <gregorywpower@gmail.com>
2019-11-08 16:28:46 -0500

    Got into the Inspections Billing Sheet

--- duplicate_printer.py
+++ duplicate_printer.py
@@ -15,4 +15,31 @@
 
 # Above is the basic principle behind this program.
 
-# Started by Gregory Power at 11/06/19 @ 4:26 PM
+# Started by Gregory Power at 11/06/19 @ 4:27 PM
+# https://blogs.harvard.edu/rprasad/2014/06/16/reading-excel-with-python-xlrd/
+
+import xlrd
+
+# Pipelines
+
+master_list = []
+comparison_list = []
+
+# Find Master Sheet
+
+workbook = xlrd.open_workbook('./2019 Inspections Billing.xlsx')
+
+# Find All of the Sheets in the Workbook
+
+print(workbook.sheet_names())
+
+# With Sheet Names, it returns a list of all of the sheets, in the file. I can use the length of the list to iterate through the Master file.
+
+print(len(workbook.sheet_names()))
+
+# Combine them all into a Single Array
+
+print(workbook.sheets())
+
+# Compare to Current Sheet
+
---
+++ steps.md
@@ -0,0 +1,11 @@
+# Duplicate Sniffer Project: Bloodhound
+
+## Steps
+
+- Organize Pipelines
+- Master Sheet
+- Current DataSheet to Query From
+
+## Outputs
+
+- Prints the duplicates out to the user.
