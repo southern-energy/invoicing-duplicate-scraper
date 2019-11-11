363a88fc3ccf4d47b78d23837505e5629d27ec73
gregorywpower <gregorywpower@gmail.com>
2019-11-11 16:13:38 -0500

    Master List now readable as list of lists

--- duplicate_printer.py
+++ duplicate_printer.py
@@ -1,3 +1,5 @@
+# Started by Gregory Power at 11/06/19 @ 4:27 PM
+
 # secnd_list = [["Not","Me"], ["Dash", "Final"], ["Not_It", "Left"]]
 # first_list = [["Dash", "Final"], ["Extra", "Data"]]
 
@@ -15,10 +17,11 @@
 
 # Above is the basic principle behind this program.
 
-# Started by Gregory Power at 11/06/19 @ 4:27 PM
+
 # https://blogs.harvard.edu/rprasad/2014/06/16/reading-excel-with-python-xlrd/
 
 import xlrd
+import pandas as pd
 
 # Pipelines
 
@@ -28,10 +31,13 @@ comparison_list = []
 # Find Master Sheet
 
 workbook = xlrd.open_workbook('./2019 Inspections Billing.xlsx')
+pd.read_excel('./2019 Inspections Billing.xlsx')
 
 # Find All of the Sheets in the Workbook
 
-print(workbook.sheet_names())
+# print(workbook.sheet_names())
+master_sheet = pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[0, 1, 2, 9])
+
 
 # With Sheet Names, it returns a list of all of the sheets, in the file. I can use the length of the list to iterate through the Master file.
 
@@ -39,7 +45,24 @@ print(len(workbook.sheet_names()))
 
 # Combine them all into a Single Array
 
-print(workbook.sheets())
+df_master = pd.concat(pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[0, 1, 2, 9], skiprows=0),sort=True, ignore_index=True)
+df_master_Street_Address_And_Service = pd.concat(pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[2, 9], skiprows=0), sort=True, ignore_index=True)
+
+# print(df_master_Street_Address_And_Service)
 
 # Compare to Current Sheet
 
+# First Have to make them into a list of lists (https://stackoverflow.com/questions/22341271/get-list-from-pandas-dataframe-column)
+
+ser_aggRows = pd.Series(df_master_Street_Address_And_Service.values.tolist())
+
+print('ser_aggRows (This collapses each row in Excel into a row this script can read.)',ser_aggRows, sep='\n', end='\n\nWe have finished organizing the rows of the Master Workbook\'s sheets into lists.\n\n\n')
+
+# first_set = set(map(tuple, ser_aggRows))
+# secnd_set = set(map(tuple, ser_aggRows))
+
+# duplicates = first_set.intersection(secnd_set)
+
+# print("Duplicates are: ")
+
+# print(duplicates)
--- steps.md
+++ steps.md
@@ -5,6 +5,12 @@
 - Organize Pipelines
 - Master Sheet
 - Current DataSheet to Query From
+- Make Each Dataframe into a List of Lists for Each Sheet
+- Compare the two lists of lists for duplicates
+- Create list of lists for duplicates
+- Print that duplicates list to user
+- (Optional) Edit the "Current DataSheet" by highlighting the indexes that correspond to the duplicates list
+- (Optional V2) Create Excel Sheet that consists of the duplciates
 
 ## Outputs
 
