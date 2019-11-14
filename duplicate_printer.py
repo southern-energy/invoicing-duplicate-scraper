66f9f72b3cfa884f5913fbebd2025de0707c680a
gregorywpower <gregorywpower@gmail.com>
2019-11-14 15:35:56 -0500

    Added modules required for xlutils + documentation

--- duplicate_printer.py
+++ duplicate_printer.py
@@ -24,6 +24,8 @@
 # https://blogs.harvard.edu/rprasad/2014/06/16/reading-excel-with-python-xlrd/
 
 import xlrd
+import xlwt
+import xlutils # Module Requires both XLRD & XLWT to be imported.
 import pandas as pd
 
 # Find Master Sheet
@@ -31,21 +33,25 @@ import pandas as pd
 workbook = xlrd.open_workbook('./2019 Inspections Billing.xlsx')
 pd.read_excel('./2019 Inspections Billing.xlsx')
 
+# =======================================================
+
+# The current_sheet variable needs to be named the sheet you want to check for duplicates.
+
+current_sheet = './11-13-19.xlsx'
+
+# =======================================================
+
 # Find All of the Sheets in the Workbook
 
-# print(workbook.sheet_names())
 master_sheet = pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[0, 1, 2, 9])
 
-
-# Combine all sheets into a single list of lists.
-
-df_master = pd.concat(pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[0, 1, 2, 9], skiprows=0),sort=True, ignore_index=True)
+# Combine all sheets of Master Sheet into a single list of lists.
 
 df_master_Street_Address_And_Service = pd.concat(pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[2, 9], skiprows=0), sort=True, ignore_index=True)
 
-# Below this line is the current sheet.
+# Read all of the sheets, using just the columns that have Street Address and the Service.
 
-df_current_sheet_Street_Address_And_Service = pd.concat(pd.read_excel('./11-7-19.xlsx', sheet_name=None, usecols=[2, 9], skiprows=0), sort=True, ignore_index=True)
+df_current_sheet_Street_Address_And_Service = pd.concat(pd.read_excel(current_sheet, sheet_name=None, usecols=[2, 9], skiprows=0), sort=True, ignore_index=True)
 
 # First Have to make them into a list of lists (https://stackoverflow.com/questions/22341271/get-list-from-pandas-dataframe-column)
 
@@ -56,7 +62,7 @@ ser_aggRows = pd.Series(df_master_Street_Address_And_Service.values.tolist())
 
 ser_aggRows_current_sheet = pd.Series(df_current_sheet_Street_Address_And_Service.values.tolist())
 
-print('Printing: ser_aggRows_current_sheet (This collapses each row in Excel into a row this script can read.)',ser_aggRows_current_sheet, sep='\n', end='\n\nWe have finished organizing the rows of the Current Workbook\'s sheets into lists.\n\n\n')
+print('Printing: ser_aggRows_current_sheet (This collapses each row in Excel into a row this script can read.)',ser_aggRows_current_sheet, sep='\n', end='\n\nWe have finished organizing the rows of the Current Workbook\'s sheets into lists.\n\n')
 
 
 first_set = set(map(tuple, ser_aggRows))
@@ -64,9 +70,13 @@ secnd_set = set(map(tuple, ser_aggRows_current_sheet))
 
 duplicates = first_set.intersection(secnd_set)
 
-if len(duplicates) < 0:
-    print("Duplicates are: \n")
-    print("Duplicates are: \n", duplicates, sep="\n", end="Please use these records to find the duplicates.")
-    print("\n\n\n\n\nWould you like to export the duplicates list as an Excel File?")
+if len(duplicates) > 0:
+    print("Duplicates are: ", duplicates, sep="\n", end="\nPlease use these records above to find the duplicates.\n")
 else: 
     print("There are no duplicates!")
+
+# Duplicates_list converts the duplicates (an object type: set, with tuples inside it, to a list of lists again)
+
+duplicates_list = list(map(list, duplicates))
+
+print(duplicates_list)
