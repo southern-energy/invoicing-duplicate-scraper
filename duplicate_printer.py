746a80a6fd1d48739209b62cb6d71a8631ccc750
gregorywpower <gregorywpower@gmail.com>
2019-11-19 14:59:55 -0500

    specified data was the dumbie data

--- duplicate_printer.py
+++ duplicate_printer.py
@@ -1,55 +1,46 @@
 # # Started by Gregory Power at 11/06/19 @ 4:27 PM
 # # Basic Functionality Achieved on 11/12/19
-
-# secnd_list_test = [["Not","Me"], ["Dash", "Final"], ["Not_It", "Left"]]
-# first_list_test = [["Dash", "Not"], ["Extra", "Data"]]
-
-# first_set_test = set(map(tuple, first_list_test))
-# secnd_set_test = set(map(tuple, secnd_list_test))
-
-# # Creates list of duplicates.
-
-# duplicates_test = first_set_test.intersection(secnd_set_test)
-
-
-# print(first_list_test)
-# print(secnd_list_test)
-# if len(duplicates_test) > 1:
-#     print(duplicates_test)
-# else:
-#     print("No duplicates have been found.")
-
-# # Above is the basic principle behind this program.
+# # Able to Print Indexes on 11/19/19
 
 import xlrd
 import xlwt
 import xlutils # Module Requires both XLRD & XLWT to be imported.
 import pandas as pd
+import numpy as np
 
-# Find Master Sheet
-
-workbook = xlrd.open_workbook('./2019 Inspections Billing.xlsx')
-pd.read_excel('./2019 Inspections Billing.xlsx')
-
+# =======================================================
 # =======================================================
 
 # The current_sheet variable needs to be named the sheet you want to check for duplicates.
 
-current_sheet = './11-13-19.xlsx'
+current_sheet = './dummySheet.xlsx'
+
+large_sheet = './2019 Inspections Billing.xlsx'
 
 # =======================================================
+# =======================================================
+# =======================================================
+# =======================================================
+# =======================================================
+
+# Find Master Sheet
+
+workbook = xlrd.open_workbook(large_sheet)
+pd.read_excel(large_sheet)
+
+
 
 # Find All of the Sheets in the Workbook
 
-master_sheet = pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[0, 1, 2, 9])
+master_sheet = pd.read_excel(large_sheet, sheet_name=None, usecols=[0, 1, 2, 9])
 
 # Combine all sheets of Master Sheet into a single list of lists.
 
-df_master_Street_Address_And_Service = pd.concat(pd.read_excel('./2019 Inspections Billing.xlsx', sheet_name=None, usecols=[2, 9], skiprows=0), sort=True, ignore_index=True)
+df_master_Street_Address_And_Service = pd.concat(pd.read_excel(large_sheet, sheet_name=None, usecols=[2, 9], skiprows=0), sort=False, ignore_index=False)
 
 # Read all of the sheets, using just the columns that have Street Address and the Service.
 
-df_current_sheet_Street_Address_And_Service = pd.concat(pd.read_excel(current_sheet, sheet_name=None, usecols=[2, 9], skiprows=0), sort=True, ignore_index=True)
+df_current_sheet_Street_Address_And_Service = pd.concat(pd.read_excel(current_sheet, sheet_name=None, usecols=[2, 9], skiprows=0), sort=False, ignore_index=False)
 
 # First Have to make them into a list of lists (https://stackoverflow.com/questions/22341271/get-list-from-pandas-dataframe-column)
 
@@ -60,11 +51,13 @@ ser_aggRows = pd.Series(df_master_Street_Address_And_Service.values.tolist())
 
 ser_aggRows_current_sheet = pd.Series(df_current_sheet_Street_Address_And_Service.values.tolist())
 
-print('Printing: ser_aggRows_current_sheet (This collapses each row in Excel into a row this script can read.)',ser_aggRows_current_sheet, sep='\n', end='\n\nWe have finished organizing the rows of the Current Workbook\'s sheets into lists.\n\n')
+# print('Printing: ser_aggRows_current_sheet (This collapses each row in Excel into a row this script can read.)',ser_aggRows_current_sheet, sep='\n', end='\n\nWe have finished organizing the rows of the Current Workbook\'s sheets into lists.\n\n')
 
 
 first_set = set(map(tuple, ser_aggRows))
 secnd_set = set(map(tuple, ser_aggRows_current_sheet))
+second_set_storage = (map(tuple, ser_aggRows_current_sheet))
+
 
 duplicates = first_set.intersection(secnd_set)
 
@@ -77,4 +70,22 @@ else:
 
 duplicates_list = list(map(list, duplicates))
 
-print(duplicates_list)
+secnd_set_list = list(map(list, second_set_storage))
+
+# print(duplicates_list)
+# print("The Length of the Second Set List: " + str(len(secnd_set_list)))
+# print("The Length of the Aggrows Sheet: " + str(len(ser_aggRows_current_sheet)))
+
+
+k = 0
+Excel_Indexes = []
+while k < len(duplicates_list):
+    # print(secnd_set_list.index(duplicates_list[k]))
+    Excel_Indexes.append(int(2) + int(secnd_set_list.index(duplicates_list[k])))
+    k += 1
+else:
+    Excel_Indexes.sort()
+    print("\nWe are done. Look to the following rows in Excel: \n", Excel_Indexes, sep="\n")
+
+
+
