1cc98d92b231ee21d1750dcc04ba2d1bda9357a5
gregorywpower <gregorywpower@gmail.com>
2019-12-08 17:48:07 -0500

    use Python 3.6.0

--- duplicate_printer.py
+++ duplicate_printer.py
@@ -1,6 +1,7 @@
 # # Started by Gregory Power at 11/06/19 @ 4:27 PM
 # # Basic Functionality Achieved on 11/12/19
 # # Able to Print Indexes on 11/19/19
+# For Environment, use Python 3.6.0
 
 import xlrd
 import xlwt
